335d54534b06052f29fee7758bfd40d50be6fbc1
nasirdiu <nasir.joypurhat@gmail.com>
2023-07-20 10:41:35 +0600

    font-end

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ Route::post('/user-login',[UserController::class,'UserLogin']);
 Route::post('/send-otp',[UserController::class,'SendOTPCode']);
 Route::post('/verify-otp',[UserController::class,'VerifyOTP']);
 Route::post('/reset-password',[UserController::class,'ResetPassword'])->middleware([TokenVerificationMiddleware::class]);
+
+
+//page Route
+Route::get('/userLogin',[UserController::class,'LoginPage']);
+Route::get('/userRegistration',[UserController::class,'RegistrationPage']);
+Route::get('/sendOtp',[UserController::class,'SendOtpPage']);
+Route::get('/verifyOtp',[UserController::class,'VerifyOTPPage']);
+Route::get('/resetPassword',[UserController::class,'ResetPasswordPage']);
+//Route::get('/dashboard',[DashboardController::class,'DashboardPage']);
+
