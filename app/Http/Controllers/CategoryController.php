a11b5bfe5ad5c3559eff4181ba97a8eb30ed0f2f
nasirdiu <nasir.joypurhat@gmail.com>
2023-07-25 16:24:41 +0600

    customare create

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+  function CategoryList(Request $request){
+
+  }
+    function CategoryCreate(Request $request){
+
+    }
+    function CategoryDelete(Request $request){
+
+    }
+    function CategoryUpdate(Request $request){
+
+    }
 }
